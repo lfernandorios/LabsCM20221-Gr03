750c9c7441b5faeb4b4aa55c667ccdf3e9d3d213
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2022-07-12 21:57:29 -0500

    Configuración Contact data activity
    Co-authored-by: Cristian Serna <mol.tired@gmail.com>
    Co-authored-by: Andres Grisales <andres.grisalesgg@gmail.com>

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20221/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20221/lab1/PersonalDataActivity.kt
@@ -1,19 +1,54 @@
 package co.edu.udea.compumovil.gr03_20221.lab1
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.helper.widget.Flow
+import java.util.*
 
 class PersonalDataActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
 
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
 
+        val pickerDateBtn = findViewById<Button>(R.id.btSelFechaN)
+        val fecha = findViewById<EditText>(R.id.fecha_nac_edit_text)
+        val siguienteBtn = findViewById<Button>(R.id.btSiguiente)
+
+        pickerDateBtn.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, myear, mmonth, mdayOfMonth ->
+                fecha.setText(""+ mdayOfMonth +"/"+ mmonth +"/"+ myear)
+            }, year, month, day)
+            datePickerDialog.show()
+        }
+
+        siguienteBtn.setOnClickListener{
+
+            //validar nombre apellidos y fecha
+
+            sendMessage()
+        }
+
+    }
+
+    private fun sendMessage() {
+        val editText = findViewById<EditText>(R.id.apellidos_edit_text)
+        val message = editText.text.toString()
+        val intent = Intent(this, ContactDataActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
