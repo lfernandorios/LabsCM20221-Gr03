5b24311c200c2c60a3b914eb7d55332df43b3507
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2022-09-15 22:12:33 -0500

    dojoFlutter

---
+++ dojoFlutter/dojo_flutter/android/app/src/main/kotlin/co/edu/udea/compumovil/gr03/dojo_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.edu.udea.compumovil.gr03.dojo_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dojoFlutter/dojo_flutter/lib/cart_state.dart
@@ -0,0 +1,19 @@
+import 'package:dojo_flutter/product.model.dart';
+import 'package:flutter/material.dart';
+
+class CartState with ChangeNotifier {
+  List<Product> items = [];
+
+  addProduct(Product product) {
+    if (items.contains(product)) {
+      return;
+    }
+    items.add(product);
+    notifyListeners();
+  }
+
+  deleteProduct(Product product) {
+    items.remove(product);
+    notifyListeners();
+  }
+}
